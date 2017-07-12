61d83909a749f397d55d510034d844333be0c523
rebeccaseel <rseelgws@gmail.com>
2017-07-12 12:02:29 -0400

    movie list practice

--- ListsPractice/Program.cs
+++ ListsPractice/Program.cs
@@ -17,7 +17,7 @@ namespace ListsPractice
             //favNumbers.Add(7); //the value of 7 is added to list, not 7 elements or indices
             //favNumbers.Add(25); //the value of 25 is added to list, not 7 element or indices
             //favNumbers.Add(30);
-            
+
             //foreach(int number in favNumbers)
             //{
             //    Console.WriteLine(number);
@@ -60,7 +60,7 @@ namespace ListsPractice
             //if the value is false print "No rain today enjoy the sun"
 
             //List<bool> rain = new List<bool> { true, false, false, true, false };
-            
+
             //foreach(bool forecast in rain)
             //{
             //    if (forecast == true)
@@ -81,9 +81,60 @@ namespace ListsPractice
             //{
             //    Console.WriteLine("I like fish too!!");
             //}
-          
 
+            //create a list with the following numbers: 1, 23,9, 77,922, 6, 32, 63, 14, 5
+            //use the contains method with the following values: 23, 77, 15
+            //remove these elements: 23 77 32 and 6
+            //use contains() again on these values: 23 77 15
+            //these will print out true, true, false, false, false, false
 
+            //List<int> numbers = new List<int> { 1, 23, 9, 77, 922, 6, 32, 63, 14, 5 };
+            //Console.WriteLine(numbers.Contains(23));
+            //Console.WriteLine(numbers.Contains(77));
+            //Console.WriteLine(numbers.Contains(15));
+
+            //numbers.Remove(23);
+            //numbers.Remove(77);
+            //numbers.Remove(32);
+            //numbers.Remove(6);
+
+            //Console.WriteLine(numbers.Contains(23));
+            //Console.WriteLine(numbers.Contains(77));
+            //Console.WriteLine(numbers.Contains(15));
+
+            //Remove.At[] will remove at a specific index
+
+            //Final problem for lists
+            //Create a console app - a movie ordering service
+            //ask the user for a movie they want to see
+            //let user know we have that movie
+            //if movie doesn't exist, add it to the list 
+            //let user know it will be available in 3-5 days
+            //when the user quits, show them all of the movies in the list
+            string movieTitle;
+            List<string> movies = new List<string> { "Transporter", "Bourne Identity", "Speed", "Star Wars", "Avatar" };
+            do
+            {
+                Console.WriteLine("Thanks for checking out our movie service!\n");
+                Console.WriteLine("You can quit at any time by typing \"QUIT\".\n");
+                Console.WriteLine("Please enter a movie to see if we have it: ");
+                movieTitle = Console.ReadLine().ToUpper();
+                if (movies.Contains(movieTitle))
+                {
+                    Console.WriteLine("Yes! We have that movie and will send it to you ASAP!");
+                }
+                else
+                {
+                    Console.WriteLine("We're sorry! We do not have that title but will add it and it will be availble in 3-5 days.");
+                    movies.Add(movieTitle);
+                }
+
+                if (Console.ReadLine() == "Quit")
+                {
+                    Console.WriteLine("Here is list of our current movies: " + movies);
+                }
+            } while (movieTitle != "QUIT");
+                        
         }
     }
 }
